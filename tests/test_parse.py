6754910612527aa84a8779bde556c1eef3f43340
Joe Cabrera <calcmaster16@gmail.com>
2014-08-12 22:28:07 -0400

    added initial tests

--- tests/test_parse.py
+++ tests/test_parse.py
@@ -1,21 +1,18 @@
 import os
-from unittest import TestCase
 import sys
-sys.path.insert(0, os.path.abspath('..'))
+sys.path.insert(0, os.path.abspath('python-xbrl'))
+import pytest
 
-import six
+try:
+    from StringIO import StringIO
+except ImportError:
+    from io import StringIO
 
 from parser import soup_maker, XBRLParser, XBRLParserException
 
-class TestParse(TestCase):
+class TestParse():
 
     def testEmptyFile(self):
-        fh = six.BytesIO(six.b(""))
-        self.assertRaises(XBRLParserException, XBRLParser.parse, fh)
+        fh = StringIO()
+        assert XBRLParser.parse(fh) is None
 
-        #def test_parse(self):
-        # make sure the shuffled sequence does not lose any elements
-
-
-if __name__ == '__main__':
-    unittest.main()
