84ea48bb52c0b909f2f5c2ca6df59a00237813eb
Joe Cabrera <calcmaster16@gmail.com>
2014-08-30 00:00:44 -0400

    added init, renamed file, setup corrected

--- setup.py
+++ setup.py
@@ -1,60 +1,22 @@
-import re
-import sys
-
-#from setuptools import setup, find_packages
 from distutils.core import setup
-
-# Read the version from __init__ to avoid importing python-xbrl while installing.
-# This lets the install work when the user does not have BeautifulSoup
-# installed.
-#VERSION = re.search(r"__version__ = '(.*?)'",
-#                    open("python-xbrl/__init__.py").read()).group(1)
-
-# Use BeautifulSoup 3 on Python 2.5 and earlier and BeautifulSoup 4 otherwise
-#if sys.version_info < (2, 6):
-#    REQUIRES = [
-#        "beautifulSoup>=3.0",
-#    ]
-#else:
-#    REQUIRES = [
-#        "beautifulsoup4"
-#    ]
-
-#if sys.version_info < (2, 7):
-#    REQUIRES.extend([
-#        "ordereddict>=1.1",
-#        "marshmallow>=0.7",
-#    ])
 
 setup(
     name='python-xbrl',
     version='1.0.1',
+    description='library for parsing xbrl documents',
     author='Joe Cabrera',
     author_email='jcabrera@eminorlabs.com',
-    keywords='xbrl, Financial, Accounting, file formats',
-    # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
-    classifiers=[
-        "Intended Audience :: Developers",
-        "Natural Language :: English",
-        "Operating System :: OS Independent",
-        "Programming Language :: Python :: 2.6",
-        "Programming Language :: Python :: 2.7",
-        "Topic :: Software Development :: Libraries :: Python Modules",
-        'Topic :: Office/Business :: Financial',
-    ],
-    install_requires=[
-	'BeautifulSoup',
-	're',
-	'marshmallow',
-	'datetime',
-	'pprint'
-	],
-    scripts=['python-xbrl/parser.py','python-xbrl/gaap.py'],
-    #packages=find_packages(exclude=['examples', 'tests']),
     url='https://github.com/greedo/python-xbrl/',
     license='Apache License',
-    description='library for parsing xbrl documents providing output as both a basic model object and serialized objects',
-    long_description=open("./README.md", "r").read(),
-    #packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
-    test_suite='tests',
-    )
+    keywords='xbrl, Financial, Accounting, file formats',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Office/Business :: Financial',
+    ],
+    scripts=['python-xbrl/python-xbrl.py'],
+)
