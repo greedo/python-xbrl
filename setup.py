c0a052b9dd06f4ad6464a15ab41a089d92444578
Joe Cabrera <calcmaster16@gmail.com>
2014-08-29 20:51:40 -0400

    update setup and manifest

--- setup.py
+++ setup.py
@@ -2,12 +2,13 @@ import re
 import sys
 
 from setuptools import setup, find_packages
+#from distutils.core import setup
 
 # Read the version from __init__ to avoid importing python-xbrl while installing.
 # This lets the install work when the user does not have BeautifulSoup
 # installed.
-VERSION = re.search(r"__version__ = '(.*?)'",
-                    open("python-xbrl/__init__.py").read()).group(1)
+#VERSION = re.search(r"__version__ = '(.*?)'",
+#                    open("python-xbrl/__init__.py").read()).group(1)
 
 # Use BeautifulSoup 3 on Python 2.5 and earlier and BeautifulSoup 4 otherwise
 if sys.version_info < (2, 6):
@@ -25,9 +26,9 @@ if sys.version_info < (2, 7):
         "marshmallow>=0.7",
     ])
 
-setup_params = dict(
+setup = dict(
     name='python-xbrl',
-    version=VERSION,
+    version='1.0.1',
     author='Joe Cabrera',
     author_email='jcabrera@eminorlabs.com',
     keywords='xbrl, Financial, Accounting, file formats',
@@ -37,6 +38,7 @@ setup_params = dict(
         "Intended Audience :: Developers",
         "Natural Language :: English",
         "Operating System :: OS Independent",
+        "Programming Language :: Python :: 2.6",
         "Programming Language :: Python :: 2.7",
         "Topic :: Software Development :: Libraries :: Python Modules",
         'Topic :: Office/Business :: Financial',
@@ -49,15 +51,14 @@ setup_params = dict(
 	'datetime',
 	'pprint'
 	],
+    scripts=['python-xbrl/parser.py','python-xbrl/gaap.py'],
+    packages=find_packages(exclude=['examples', 'tests']),
     url='https://github.com/greedo/python-xbrl/',
     license='Apache License',
     description='library for parsing xbrl documents providing output as both a basic model object and serialized objects',
-    long_description=open("./README", "r").read(),
-    packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+    long_description=open("./README.md", "r").read(),
+    #packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
     include_package_data=True,
     zip_safe=True,
     test_suite='tests',
     )
-
-if __name__ == '__main__':
-    setup(**setup_params)
