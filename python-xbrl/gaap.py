01ae543bf8ac1bf16a3647d5f1b3aba37d1e13b0
Joe Cabrera <calcmaster16@gmail.com>
2014-08-30 15:18:44 -0400

    strange

--- python-xbrl/gaap.py
+++ python-xbrl/gaap.py
@@ -6,7 +6,7 @@ xbrl_parser = XBRLParser(precision=0)
 xbrl = xbrl_parser.parse(file("sam-20140329.xml"))
 
 # Parse just the GAAP data from the xbrl object
-gaap_obj = xbrl_parser.parseGAAP(xbrl, doc_date="20140329")
+gaap_obj = xbrl_parser.parseGAAP(xbrl, doc_date="20140329", doc_type="10-Q"))
 
 # Serialize the GAAP data
 serialized = GAAPSerializer(gaap_obj)
