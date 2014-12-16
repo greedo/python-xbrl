3367a4ed039522cf70be755b7822c1f25c9a3c8b
Joe Cabrera <calcmaster16@gmail.com>
2014-12-16 00:43:14 -0500

    updated example

--- examples/gaap.py
+++ examples/gaap.py
@@ -1,18 +1,21 @@
+from __future__ import print_function
+
 from xbrl import XBRLParser, GAAP, GAAPSerializer
 
 xbrl_parser = XBRLParser(precision=0)
 
 # Parse an incoming XBRL file
-xbrl = xbrl_parser.parse(file("../tests/sam-20140329.xml"))
+xbrl = xbrl_parser.parse(file("../tests/sam-20130629.xml"))
 
 # Parse just the GAAP data from the xbrl object
 gaap_obj = xbrl_parser.parseGAAP(xbrl,
-                                 doc_date="20140329",
-                                 doc_type="10-Q",
-                                 context="current")
+                                 doc_date="20130629",
+                                 doc_type="10-K",
+                                 context="current",
+                                 ignore_errors=0)
 
 # Serialize the GAAP data
 serialized = GAAPSerializer(gaap_obj)
 
 # Print out the serialized GAAP data
-print serialized.data
+print (serialized.data)
