c050648af6a8e862c4cbf4f7f15ee3d54a92a545
Joe Cabrera <calcmaster16@gmail.com>
2014-12-17 20:11:49 -0500

    fixed one file

--- xbrl/gaap.py
+++ xbrl/gaap.py
@@ -5,12 +5,11 @@ from xbrl import XBRLParser, GAAP, GAAPSerializer
 xbrl_parser = XBRLParser(0)
 
 # Parse an incoming XBRL file
-#xbrl = xbrl_parser.parse(file("../tests/sam-20131228.xml"))
-xbrl = xbrl_parser.parse(file("sam-20140927.xml"))
+xbrl = xbrl_parser.parse(file("tests/sam-20131228.xml"))
 
 # Parse just the GAAP data from the xbrl object
 gaap_obj = xbrl_parser.parseGAAP(xbrl,
-                                 doc_date="20140927",
+                                 doc_date="20131228",
                                  doc_type="10-K",
                                  context="current",
                                  ignore_errors=0)
@@ -19,9 +18,4 @@ gaap_obj = xbrl_parser.parseGAAP(xbrl,
 serialized = GAAPSerializer(gaap_obj)
 
 # Print out the serialized GAAP data
-#print(serialized.data)
-
-for k, v in serialized.data.items():
-    print(v)
-    with open('d793237d10q.htm', 'r') as f:
-        print f.read()
+print(serialized.data)
